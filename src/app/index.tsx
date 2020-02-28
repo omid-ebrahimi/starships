6ea83112c370ea75d8e6558993a8da77f1d4cf6d
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2020-02-28 23:33:56 +0330

    show logo while loading (should be centered)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Cell, Grid, Row } from '@material/react-layout-grid';
+import logo from '../logo.svg';
 import CardShip from './CardShip';
+import styles from './app.module.css';
 import { useStarShips } from './controller';
 
 function App(): JSX.Element {
     const { data } = useStarShips();
-    return (
-        <Grid>
-            <Row>
-                <Cell desktopColumns={3}> </Cell>
-                <Cell desktopColumns={6} tabletColumns={8} phoneColumns={4}>
-                    {data && data.results.map(ship => <CardShip key={ship['name']} {...ship} />)}
-                </Cell>
-                <Cell desktopColumns={3}> </Cell>
-            </Row>
-        </Grid>
-    );
+    if (data) {
+        return (
+            <Grid>
+                <Row>
+                    <Cell desktopColumns={3}> </Cell>
+                    <Cell desktopColumns={6} tabletColumns={8} phoneColumns={4}>
+                        {data && data.results.map(ship => <CardShip key={ship['name']} {...ship} />)}
+                    </Cell>
+                    <Cell desktopColumns={3}> </Cell>
+                </Row>
+                <Row className={styles.footer}>
+                    <p>Previous</p>
+                    <p>Next</p>
+                </Row>
+            </Grid>
+        );
+    }
+    return <img src={logo} className={styles.logo} alt="logo" />;
 }
 
 export default App;
