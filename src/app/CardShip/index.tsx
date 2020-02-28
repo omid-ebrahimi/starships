2b44a0d7896257b835a024ae09fd9180f88d8597
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2020-02-28 19:57:36 +0330

    improves CardShip

--- src/app/CardShip/index.tsx
+++ src/app/CardShip/index.tsx
@@ -1,11 +1,23 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React from 'react';
 import { StarShip } from '../../api/types';
+import { capitalizeFirstLetter } from '../../utils/string';
 import styles from './cardShip.module.css';
 
-function CardShip({ name }: StarShip): JSX.Element {
+function CardShip({ name, crew, passengers, hyperdrive_rating }: StarShip): JSX.Element {
     return (
         <section className={styles.container}>
-            <h3>{name}</h3>
+            <h4>{capitalizeFirstLetter(name)}</h4>
+            <div className={styles.content}>
+                <div className={styles.stats}>
+                    <span>Crew: {parseInt(crew) ? crew : 'None'}</span>
+                    <p>Passengers: {parseInt(passengers) ? passengers : 'None'}</p>
+                </div>
+                <div className={styles.rate}>
+                    <span>Hyperdrive Class</span>
+                    <p>{hyperdrive_rating}</p>
+                </div>
+            </div>
         </section>
     );
 }
