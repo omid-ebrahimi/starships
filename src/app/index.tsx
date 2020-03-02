2ab9dbce98c92fa1a058d35fbb7fc64ebcbd554b
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2020-03-02 12:45:10 +0330

    adds footer to the app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Cell, Grid, Row } from '@material/react-layout-grid';
 import { ButtonWarning, ButtonSuccess } from '../components/Button';
 import Ships from './Ships';
@@ -11,23 +11,25 @@ function App(): JSX.Element {
     if (data) {
         const { results, next, previous } = data;
         return (
-            <Grid>
-                <Row>
-                    <Cell desktopColumns={3}> </Cell>
-                    <Cell desktopColumns={6} tabletColumns={8} phoneColumns={4}>
-                        <Ships ships={results} loading={loading} />
-                    </Cell>
-                    <Cell desktopColumns={3}> </Cell>
-                </Row>
-                <Row className={styles.footer}>
+            <Fragment>
+                <Grid>
+                    <Row>
+                        <Cell desktopColumns={3}> </Cell>
+                        <Cell desktopColumns={6} tabletColumns={8} phoneColumns={4}>
+                            <Ships ships={results} loading={loading} />
+                        </Cell>
+                        <Cell desktopColumns={3}> </Cell>
+                    </Row>
+                </Grid>
+                <footer className={styles.footer}>
                     <ButtonWarning onClick={(): void => loadData(previous)} disabled={loading || !previous}>
                         Previous
                     </ButtonWarning>
                     <ButtonSuccess onClick={(): void => loadData(next)} disabled={loading || !next}>
                         Next
                     </ButtonSuccess>
-                </Row>
-            </Grid>
+                </footer>
+            </Fragment>
         );
     }
     return <Splash />;
